def6ba564332df78a16bcc2acb45de9e4328bb64
Jose <joseoctavioconti@gmail.com>
2020-06-14 18:31:13 -0300

    Config routes

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <div className="internal" />
+      <div className="internal" />
+      <div className="internal" />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background: blue;
+  display: flex;
+
+  div {
+    width: 50px;
+    height: 50px;
+    background: #ff0000;
+  }
+`;
